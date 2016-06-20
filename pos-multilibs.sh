b6f1552ee62513c393e20a04db8f2c5a0d3cef3a
ericfernandesferreira <candelabrus@gmail.com>
2016-06-20 15:53:58 -0300

    multilibs pos installation

--- pos-multilibs.sh
+++ pos-multilibs.sh
@@ -13,9 +13,6 @@ wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/lib
 wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/poppler-0.44.0-i586-1.txz
 
 # Convertendo para compat32
-su -
-. /etc/profile.d/32dev.sh
-cd /home/backup
 convertpkg-compat32 -i GConf-3.2.6-i586-3.txz
 convertpkg-compat32 -i librsvg-2.40.15-i586-2.txz
 convertpkg-compat32 -i libspectre-0.2.7-i486-1.txz
@@ -23,3 +20,7 @@ convertpkg-compat32 -i poppler-0.44.0-i586-1.txz
 
 # Instalando o restante do multilibs
 upgradepkg --reinstall --install-new /tmp/*.t?z
+
+# Movendo os novos arquivos compat32 para a pasta principal do multilibs
+mkdir -p /home/backup/multilibs/slackware64-compat32/xx-compat32
+mv /tmp/*.t?z /home/backup/multilibs/slackware64-compat32/xx-compat32
