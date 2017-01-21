a0bab7085add4b4fdbd25ceb4c2d97fbb3c8839c
ericfernandesferreira <candelabrus@gmail.com>
2017-01-20 23:02:55 -0200

    some fixes

--- multilibs-14.2.sh
+++ multilibs-14.2.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 # Baixando o multilibs 14.2
-cd /home/backup/multilibs
+cd /home/backup/slackware
 lftp -c 'open http://www.slackware.com/~alien/multilib/ ; mirror -c -e 14.2'
+mv 14.2 multilibs
 
 # Removendo o desnecessário (XAP)
-cd 14.2/slackware64-compat32
+cd multilibs/slackware64-compat32
 rm -rf xap-compat32
 cd ..
 
-# Instalando o multilibs (somente gcc)
+# Instalando o multilibs
 upgradepkg --reinstall --install-new *.t?z
-# upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
+upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
--- pos-multilibs-14.2.sh
+++ pos-multilibs-14.2.sh
@@ -6,11 +6,10 @@ rm -rf *
 
 # Baixando o restante necessário para o multilibs
 # GConf,librsvg,libspectre e poppler
-cd /home/backup
-wget -c http://mirrors.slackware.com/slackware/14.2/slackware/l/GConf-3.2.6-i586-3.txz
-wget -c http://mirrors.slackware.com/slackware/14.2/slackware/l/librsvg-2.40.16-i586-1.txz
-wget -c http://mirrors.slackware.com/slackware/14.2/slackware/l/libspectre-0.2.7-i486-1.txz
-wget -c http://mirrors.slackware.com/slackware/14.2/slackware/l/poppler-0.45.0-i586-1.txz
+wget -c http://mirrors.slackware.com/slackware/slackware-14.2/slackware/l/GConf-3.2.6-i586-3.txz
+wget -c http://mirrors.slackware.com/slackware/slackware-14.2/slackware/l/librsvg-2.40.16-i586-1.txz
+wget -c http://mirrors.slackware.com/slackware/slackware-14.2/slackware/l/libspectre-0.2.7-i486-1.txz
+wget -c http://mirrors.slackware.com/slackware/slackware-14.2/slackware/l/poppler-0.45.0-i586-1.txz
 
 # Convertendo para compat32
 convertpkg-compat32 -i GConf-3.2.6-i586-3.txz
@@ -22,5 +21,5 @@ convertpkg-compat32 -i poppler-0.45.0-i586-1.txz
 upgradepkg --reinstall --install-new /tmp/*.t?z
 
 # Movendo os novos arquivos compat32 para a pasta principal do multilibs
-mkdir -p /home/backup/multilibs/slackware64-compat32/xx-compat32
+mkdir -p /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
 mv /tmp/*.t?z /home/backup/multilibs/slackware64-compat32/xx-compat32
