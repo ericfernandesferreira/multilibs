d944e52d80b881a502c66312dc480f34dd3826c0
ericfernandesferreira <candelabrus@gmail.com>
2017-03-27 03:55:35 -0300

    multilibs for current updated

--- multilibs-current.sh
+++ multilibs-current.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 # Baixando o multilibs current
-cd /home/backup/multilibs
 lftp -c 'open http://www.slackware.com/~alien/multilib/ ; mirror -c -e current'
+cd ..
+mv multilibs multi
+cd multi
+mv current /home/backup/slackware/multilibs
 
 # Removendo o desnecessário (XAP)
-cd current/slackware64-compat32
+cd /home/backup/slackware/multilibs/slackware64-compat32
 rm -rf xap-compat32
 cd ..
 
-# Instalando o multilibs (somente gcc)
+# Instalando o multilibs
 upgradepkg --reinstall --install-new *.t?z
-# upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
+upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
--- pos-multilibs-current.sh
+++ pos-multilibs-current.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-# Limpando o arquivo temporário
-cd /tmp
-rm -rf *
+GConf=GConf-3.2.6-i586-3.txz
+librsvg=librsvg-2.40.16-i586-1.txz
+libspectre=libspectre-0.2.7-i486-1.txz
+poppler=poppler-0.50.0-i586-1.txz
 
 # Baixando o restante necessário para o multilibs
 # GConf,librsvg,libspectre e poppler
-cd /home/backup
-wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/GConf-3.2.6-i586-3.txz
-wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/librsvg-2.40.16-i586-1.txz
-wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/libspectre-0.2.7-i486-1.txz
-wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/poppler-0.45.0-i586-1.txz
+cd /home/backup/slackware
+mkdir xx-compat32
+cd xx-compat32
+wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/$GConf
+wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/$librsvg
+wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/$libspectre
+wget -c http://mirrors.slackware.com/slackware/slackware-current/slackware/l/$poppler
 
 # Convertendo para compat32
-convertpkg-compat32 -i GConf-3.2.6-i586-3.txz
-convertpkg-compat32 -i librsvg-2.40.16-i586-1.txz
-convertpkg-compat32 -i libspectre-0.2.7-i486-1.txz
-convertpkg-compat32 -i poppler-0.45.0-i586-1.txz
-
-# Instalando o restante do multilibs
-upgradepkg --reinstall --install-new /tmp/*.t?z
+convertpkg-compat32 -i $GConf
+convertpkg-compat32 -i $librsvg
+convertpkg-compat32 -i $libspectre
+convertpkg-compat32 -i $poppler
 
 # Movendo os novos arquivos compat32 para a pasta principal do multilibs
-mkdir -p /home/backup/multilibs/slackware64-compat32/xx-compat32
-mv /tmp/*.t?z /home/backup/multilibs/slackware64-compat32/xx-compat32
+mkdir -p /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+mv /tmp/GConf-compat32-3.2.6-x86_64-3compat32.txz /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+mv /tmp/librsvg-compat32-2.40.16-x86_64-1compat32.txz /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+mv /tmp/libspectre-compat32-0.2.7-x86_64-1compat32.txz /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+mv /tmp/poppler-compat32-0.50.0-x86_64-1compat32.txz /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+
+# Instalando o restante do multilibs
+cd /home/backup/slackware/multilibs/slackware64-compat32/xx-compat32
+upgradepkg --reinstall --install-new *.t?z
