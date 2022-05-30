f522885fd17f2b820cfb0d7e0e85603ad5567d2b
Eric Fernandes Ferreira <candelabrus@gmail.com>
2022-05-30 00:29:04 -0300

    multilibs for slackware 15.0

--- README.txt
+++ README.txt
@@ -1,3 +1,1 @@
-Esse é um script que automatiza a instalação do Multilibs no Slackware64 14.2 e Current
-
-O arquivo "pos-multilibs.sh" instala mais 4 pacotes multilibs para poder compilar o infinality-fonts em 32bits
+Esse é um script que automatiza a instalação do Multilibs no Slackware64 15.0 ou Current
--- multilibs-15.0.sh
+++ multilibs-15.0.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Baixando o multilibs current
+# Baixando o multilibs 15.0
 cd /home/backup/slackware
 rm -rf multilibs
-lftp -c 'open http://www.slackware.com/~alien/multilib/ ; mirror -c -e current'
-mv current multilibs
+lftp -c 'open http://www.slackware.com/~alien/multilib/ ; mirror -c -e 15.0'
+mv 15.0 multilibs
 cd multilibs
 
-# Instalando o multilibs
+# Instalando o multilibs 15.0
 upgradepkg --reinstall --install-new *.t?z
 upgradepkg --install-new slackware64-compat32/*-compat32/*.t?z
